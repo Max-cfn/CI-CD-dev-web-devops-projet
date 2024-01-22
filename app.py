3bac2dbc8e1ce8c0b039168959fb4bfea461caa6
ThbLoth <thibault.loth@efrei.net>
2024-01-22 15:03:49 +0100

    Ajout d'un bug pour tester l'arrêt du workflow

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World! This is my web app.'
+    #return 'Hello, World! This is my web app.'
+    return 'Hello, World! Cette ligne fera bug le workflow'
 
 if __name__ == '__main__':
     app.run(debug=True)
