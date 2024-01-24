02d38e22ca62669faf1edaecc2f3e4ad15954ded
ThbLoth <thibault.loth@efrei.net>
2024-01-24 14:29:36 +0100

    Ajout d'une page about pour tester la mise à jour du site

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ def home():
     return 'Hello, World! This is my web app.'
     #return 'Hello, World! Cette ligne fera bug le workflow'
 
+@app.route('/about')
+def about():
+    return 'This is the about page.'
+
 if __name__ == '__main__':
     app.run(debug=True)
