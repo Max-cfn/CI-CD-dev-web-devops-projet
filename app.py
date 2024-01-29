7b8bceb31a16a0c056ec09e430ca1be5cc3c431c
ThbLoth <73190082+ThbLoth@users.noreply.github.com>
2024-01-29 17:27:41 +0100

    edit app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World! This is my web app. Made by Thibault LOTH, Keenan BARANES and Maxence CERFONTAINE'
-    #return 'Hello, World! Cette ligne fera bug le workflow'
+    return 'Hello, World! This is my web app.'
 
 @app.route('/about')
 def about():
     return 'This is the about page. Please contact us through mail'
+    #return 'Hello, World! Cette ligne fera bug le workflow'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
